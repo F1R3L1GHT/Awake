f7d9485e82a5d827c25accdf6415809d96d47961
Glitch (hello-express) <none>
2019-05-08 17:20:13 +0000

    🏇🚣 Checkpoint ./ping.js:1326143/859 ./data/urls.json:1326143/20

--- ping.js
+++ ping.js
@@ -1,10 +1,34 @@
 const fs = require("fs");
 const http = require("http");
 
+function pingURL(url)
+{
+ return new Promise((res, rej) => {
+   
+ });
+}
+
 function ping()
 {
-  fs.readFile('DATA', 'utf8', function(err, contents) {
-    if (!err
+  fs.readFile("data/urls.json", "utf8", function(err, contents) {
+    if (!err)
+    {
+      try {
+        let j = JSON.parse(contents);
+        
+        j.forEach((url) => {
+          url.then(() => {
+            console.log(`Sucessfully pinged ${url}!`);
+          }).catch((status) => {
+            console.log(`Unable to ping ${url}! Status code: ${status}.`);
+          });
+        });
+      } catch(e) {
+        return;
+      }
+    } else {
+     return; 
+    }
   });
 }
 
